df94c66567331542761368eb0030f658755bbb6d
marko shenouda <136264452+Markoshenouda@users.noreply.github.com>
2025-06-18 18:42:19 +0300

    Update ai_logic.h

--- include/ai_logic.h
+++ include/ai_logic.h
@@ -1,16 +1,26 @@
+/*
+ * ai_logic.h
+ *
+ *  Created on: Apr 28, 2025
+ *      Author: Mariam Samy
+ */
 
 #ifndef AI_LOGIC_H
 #define AI_LOGIC_H
 
 #include <vector>
 #include "game_constants.h"
-struct Node; 
-class AILogic {
+
 class AILogic {
 public:
+    static bool isMoveValid(const std::vector<int>& used, int num);
     static void easyAI(const std::vector<char>& board, std::vector<int>& used, int& choice);
-    static void mediumAI(const std::vector<char>& board, std::vector<int>& used, int& choice, char aiSymbol, char playerSymbol);
+    static void mediumAI(const std::vector<char>& board, std::vector<int>& used, int& choice,
+                        char aiSymbol, char playerSymbol);
     static void hardAI(const std::vector<char>& board, std::vector<int>& used, int& choice, char aiSymbol);
-    static int minimax(std::vector<char> board, bool isMaximizing, char aiSymbol, char playerSymbol); // 👈 Add this
-    static int minimaxOnTree(Node* node, bool isMaximizing, int alpha, int beta); // assumed already declared
+    static int minimax(std::vector<char> board, bool isMaximizing, char aiSymbol, char playerSymbol);
+
+private:
 };
+
+#endif
