c78e946ff37c5c3119c94d209a4974157b7c50e6
marko shenouda <136264452+Markoshenouda@users.noreply.github.com>
2025-06-18 18:25:52 +0300

    Update ai_logic.h

--- include/ai_logic.h
+++ include/ai_logic.h
@@ -4,17 +4,17 @@
 
 #include <vector>
 #include "game_constants.h"
-
+struct Node; 
 class AILogic {
 public:
     static bool isMoveValid(const std::vector<int>& used, int num);
     static void easyAI(const std::vector<char>& board, std::vector<int>& used, int& choice);
     static void mediumAI(const std::vector<char>& board, std::vector<int>& used, int& choice,
-                        char aiSymbol, char playerSymbol);
+                         char aiSymbol, char playerSymbol);
     static void hardAI(const std::vector<char>& board, std::vector<int>& used, int& choice, char aiSymbol);
-static int minimaxOnTree(struct Node* node, bool isMaximizing, int alpha, int beta);
 
-private:
+    // 🆕 Correct declaration of the actual used function
+    static int minimaxOnTree(Node* node, bool isMaximizing, int alpha, int beta);
 };
 
 #endif
